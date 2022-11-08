8f712dd29b1893c0e97e0bb2298b65167cc93c32
Nur Ismam Bayu Wijaya <mk.nur.wijaya@pertamina.com>
2022-11-08 11:16:16 +0700

    Add project files.

---
+++ FrontEnd/Controllers/TransaksiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Controllers
+{
+    public class TransaksiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddHousehold()
+        {
+            return View();
+        }
+        public IActionResult HouseholdCalc() 
+        {
+            return View();
+        }
+
+        public IActionResult ListCar()
+        {
+            return View();
+        }
+
+        public IActionResult AddCar()
+        {
+            return View();
+        }
+
+        public IActionResult ListTree()
+        {
+            return View();
+        }
+
+        public IActionResult AddTree()
+        {
+            return View();
+        }
+    }
+}
---
+++ FrontEnd/wwwroot/js/site.js
@@ -0,0 +1,81 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    $('#datepicker').datepicker({
+        format: 'MM-yyyy',
+        startView: "months",
+        minViewMode: "months"
+    });
+});
+
+//$(document).ready(function () {
+//    $('#datepicker').on('change', function () {
+//        var dateThis = $(this).val();
+//        $('#tanggal_name').text(dateThis);
+//    });
+//});
+
+$(document).ready(function () {
+    $('#datepicker').on('change', function () {
+        var dateThis = $(this).val();
+        $('#tanggal_name1').text(dateThis);
+    });
+});
+
+$(document).ready(function () {
+    $('#ubah').on('change', function () {
+        var dateThis = $(this).val();
+        $('#nav-periode-tab').text(dateThis);
+    });
+});
+
+
+$(document).ready(function () {
+    $('#hoesehold').DataTable({
+        "ajax": '/other/array.json',
+        "columns": [
+            { "data": 'name' },
+            { "data": 'position' },
+            { "data": 'office' },
+            { "data": 'extn' },
+            { "data": 'start_date' },
+            { "data": 'salary' },
+        ],
+    });
+});
+
+$(document).ready(function () {
+    $('#vehicle').DataTable({
+        "ajax": '/other/array.json',
+        "columns": [
+            { "data": 'name' },
+            { "data": 'position' },
+            { "data": 'office' },
+            { "data": 'extn' },
+            { "data": 'start_date' },
+            { "data": 'salary' },
+        ],
+    });
+});
+
+$(document).ready(function () {
+    $('#carbonabs').DataTable({
+        "ajax": '/other/array.json',
+        "columns": [
+            { "data": 'name' },
+            { "data": 'position' },
+            { "data": 'office' },
+            { "data": 'extn' },
+            { "data": 'start_date' },
+            { "data": 'salary' },
+        ],
+    });
+});
+
+$('a[idSpan="1"]').on('click', function () {
+    var subText = $(this).attr("spanName");
+    console.log(subText);
+    $('#subtext').text(subText);
+})
